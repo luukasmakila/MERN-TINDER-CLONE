844c0098842594120fdbef2b6257361387ab644a
Luukas <luukas.makila@kolumbus.fi>
2022-01-05 01:55:10 +0200

    introduced react router and started add user page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,14 +3,17 @@ import '../styles/Header.css'
 import PersonIcon from '@mui/icons-material/Person'
 import ChatIcon from '@mui/icons-material/Chat'
 import { IconButton } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 
 const Header = () => {
   return (
     <div className='header'>
-      <IconButton>
-        <PersonIcon fontSize='large' className='header_icon'/>
-      </IconButton>
+      <Link to='/add-user'>
+        <IconButton>
+          <PersonIcon fontSize='large' className='header_icon'/>
+        </IconButton>
+      </Link>
       <img 
         className='header_logo'
         src='https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-Flame-Logo.wine.svg'
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import AddUser from "./components/AddUser"
 import './styles/index.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-ReactDOM.render(<App />,document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/add-user' element={<AddUser/>}/>
+    </Routes>
+  </Router>,
+  document.getElementById('root')
+)
