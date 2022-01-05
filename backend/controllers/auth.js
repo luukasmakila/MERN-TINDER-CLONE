dd962b275dbc22ae389c9f30deabad615a8f4f0a
Luukas <luukas.makila@kolumbus.fi>
2022-01-05 20:17:13 +0200

    created the login function

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,15 +1,11 @@
 const User = require('../models/user')
-const bcrypt = require('bcrypt')
 
 const sign_up = async (request, response, next) => {
   const body = request.body
 
-  const passwordSalt = 10
-  const passwordHash = await bcrypt.hash(body.password, passwordSalt)
-
   const user = new User({
     email: body.email,
-    password: passwordHash,
+    password: body.password,
     name: body.name,
     imgUrl: body.imgUrl,
     bio: body.bio
@@ -23,13 +19,27 @@ const sign_up = async (request, response, next) => {
   }
 }
 
-const login = (request, response, next) => {
-  response.send('login route')
+const login = async (request, response, next) => {
+  const {email, password} = request.body
+
+  if (!email || !password) {
+    response.status(400).json({success: false, error: 'Email and password needed'})
+  }
 
   try {
-    
+    const user = await User.findOne({ email }).select('+password') //<-- password refers to the "this.password"
+    if (!user) {
+      response.status(404).json({success: false, error: 'Invalid credentials'})
+    }
+    const passwordsMatch = await user.checkPassword(password)
+
+    if(!passwordsMatch) {
+      response.status(404).json({success: false, error: 'Invalid credentials'})
+    }
+
+    response.status(200).json({success: true, token: 'd243adfgf96df2ehf'})
   } catch (error) {
-    
+    response.status(500).json({success: false, error: error.message})
   }
 }
 
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = mongoose.Schema({
   email: String,
@@ -8,4 +9,16 @@ const userSchema = mongoose.Schema({
   bio: String
 })
 
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) { //if password is hashed dont hash it again
+    next()
+  }
+  const passwordSalt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, passwordSalt) //makes the keyword password = hashed password
+  next()
+})
+
+userSchema.methods.checkPassword = async function(password) {
+  return await bcrypt.compare(password, this.password) //<-- Users saved password hashed
+}
 module.exports = mongoose.model('User', userSchema)
